6dfe0327d4f0fb1179a12710f88b966ca72df00e
Christopher Chan <36608720+chrischank@users.noreply.github.com>
2020-05-13 21:51:31 +0200

    Changed to RStoolbox

--- Indices.R
+++ Indices.R
@@ -1,8 +1,8 @@
-#########################
-#Index Creation Würzburg#
-#       04/05/2020      #
-#Author: Chris Chan     #
-#########################
+##########################
+#Index Creation Wuerzburg#
+#        04/05/2020      #
+#Author: Chris Chan      #
+##########################
 
 library(RStoolbox)
 library(tidyverse)
@@ -13,6 +13,7 @@ library(rgdal)
 library(raster)
 library(rasterVis)
 library(viridis)
+library(cluster)
 
 setwd("C:/Users/s1526/Dropbox/MB3_Scripts")
 dir.create("C:/Users/s1526/Dropbox/MB3_Scripts/indices")
@@ -28,6 +29,8 @@ plot(Würzburg_ROI)
 
 #LOAD bands for IBI and RGB
 setwd(Data_path)
+list.files()
+
 b11_MIR <- raster(list.files()[1])
 b8_NIR <- raster(list.files()[5])
 b4_RED <- raster(list.files()[4])
@@ -37,8 +40,7 @@ b2_BLUE <- raster(list.files()[2])
 #Create RGB stack
 Wü_RGB <- stack(b4_RED, b3_GREEN, b2_BLUE)
 ggRGB(Wü_RGB, r=1, g=2, b=3, stretch="hist")
-writeRaster(Wü_RGB, "Data/Wü_RGB.tif", format="GTiff", overwrite=TRUE)
-
+writeRaster(Wü_RGB, "Wü_RGB.tif", format="GTiff", overwrite=TRUE)
 
 #################
 #INDEX FUNCTIONS#
@@ -57,29 +59,31 @@ IBI <- function(MIR, NIR, GREEN, RED){
 #NDVI----
 #band 8, band 4
 
-NDVI <- function(NIR, RED){
-  ndvi <- (NIR-RED)/
-    (NIR+RED)
-  return(ndvi)
-}
+#NDVI <- function(NIR, RED){
+#  ndvi <- (NIR-RED)/
+#    (NIR+RED)
+#  return(ndvi)
+#}
 
 #NDWI----
-NDWI <- function(GREEN, NIR){
-  ndwi <- (GREEN-NIR)/
-    (GREEN+NIR)
-  return(ndwi)
-}
+#NDWI <- function(GREEN, NIR){
+#  ndwi <- (GREEN-NIR)/
+#    (GREEN+NIR)
+#  return(ndwi)
+#}
 
 #Disaggregate b11 from 20m res to 10m fact=2 res for calculation
 DissAgg.b11_MIR <- disaggregate(b11_MIR, fact=2)
 
+all_band <- stack(b2_BLUE, b3_GREEN, b4_RED, b8_NIR, DissAgg.b11_MIR)
+
 Wü_IBI <- IBI(DissAgg.b11_MIR, b8_NIR, b3_GREEN, b4_RED)
 plot(Wü_IBI)
 
-Wü_NDVI <- NDVI(b8_NIR, b4_RED)
+Wü_NDVI <- spectralIndices(all_band, blue=1, green=2, red=3, nir=4, swir2=5, scaleFactor = 1, index="NDVI")
 plot(Wü_NDVI)
 
-Wü_NDWI <- NDWI(b3_GREEN, b8_NIR)
+Wü_NDWI <- spectralIndices(all_band, blue=1, green=2, red=3, nir=4, swir2=5, scaleFactor = 1, index="NDWI")
 plot(Wü_NDWI)
 
 #Mask ROI of IBI
@@ -102,4 +106,4 @@ writeRaster(Mask_Wü_NDWI, "Wü_ROI_NDWI.tif", format="GTiff", overwrite=TRUE)
 
 stack_ROI_indices <- stack(Mask_Wü_IBI, Mask_Wü_NDVI, Mask_Wü_NDWI)
 writeRaster(stack_ROI_indices, "stack_ROI_indices.tif", format="GTiff", overwrite=TRUE)
-
+writeRaster(stack_ROI_indices, "stack_ROI_indices.grd", format="raster", overwrite=TRUE)
