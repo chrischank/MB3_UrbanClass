11053b1683f0d60e3af61c9b4bb6e7f13d3e6f95
Christopher Chan <36608720+chrischank@users.noreply.github.com>
2020-06-14 12:39:09 +0200

    TOC

---
+++ TOC.R
@@ -0,0 +1,13 @@
+##################################
+#Total Operating Characteristiscs#
+##################################
+
+#Testing TOC for RF RGB data between 2 prediction mode 80/20
+
+setwd("C:/Users/s1526/Dropbox/MB3_Scripts/Data/RF-QGIS")
+
+library(TOC)
+library(tidyverse)
+library(rgdal)
+
+
