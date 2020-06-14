364547b9eee39c0b0150c1e7477df2f9c895fbf1
Christopher Chan <36608720+chrischank@users.noreply.github.com>
2020-06-14 13:12:13 +0200

    Prediction

--- supervised_class.R
+++ supervised_class.R
@@ -1,12 +1,17 @@
 library(raster)
 library(RStoolbox)
 library(rgdal)
-install.packages("e1071")
 library(e1071)
 library(randomForest)
-setwd("C:/Users/chofi/Documents/2019/Maestria/FieldMeasurements/Class")
-allband<-raster("C:/Users/chofi/Dropbox/MB3_Scripts/Data/Wü_RGB.tif")
-td<-readOGR("shapes_class/trainpoly_merged.shp")
+library(tidyverse)
+library(raster)
+library(rasterVis)
+
+setwd("C:/Users/s1526/Dropbox/MB3_Scripts/Data/RF-QGIS")
+
+allband <- raster("C:/Users/s1526/Dropbox/MB3_Scripts/Data/Wü_RGB.tif")
+
+td <- readOGR("C:/Users/s1526/Dropbox/MB3_Scripts/Data/Training")
 
 plot(allband[[1]])
 plot(td,add=TRUE)
@@ -14,10 +19,15 @@ plot(td,add=TRUE)
 sc2<-superClass(allband,trainData=td,
                responseCol = "Class_id",
                model = "rf", 
-               tuneLength = 1,
-               trainPartition = 0.70,
+               tuneLength = "random",
+               trainPartition = 0.75,
                filename="Class_test2.tif"  
                )
+
 plot(sc2$map)
+
 sc2$validation$performance
 
+output_class_3 <- raster::predict(allband, sc2$model, filename="output_class_3", format="GTiff", overwrite=TRUE)
+
+plot(output_class_3)
